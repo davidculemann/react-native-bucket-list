30171395d8e6f90a4508df3e9b9775eff2208254
davidculemann <davidculemann@gmail.com>
2023-12-30 21:53:35 +0400

    add create new item button

--- src/navigator/tab/Tab.tsx
+++ src/navigator/tab/Tab.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AntDesign } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { colors } from '@theme';
-import { TabParamList, TabBarStatus } from './Tab.typeDefs';
+import React from 'react';
+import { Image, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { HomeStackNavigator, ProfileStackNavigator } from '../stack/Stack';
+import { TabBarStatus, TabParamList } from './Tab.typeDefs';
 
 const Tab = createBottomTabNavigator<TabParamList>();
 
@@ -30,6 +32,16 @@ export default function TabNavigator() {
       })}>
       <Tab.Screen name="HomeTab" component={HomeStackNavigator} options={{ title: 'Home' }} />
       <Tab.Screen
+        name="Create"
+        component={HomeStackNavigator}
+        options={{
+          tabBarIcon: () => {
+            return <AddItemButton />;
+          },
+          presentation: 'modal',
+        }}
+      />
+      <Tab.Screen
         name="ProfileTab"
         component={ProfileStackNavigator}
         options={{ title: 'Profile' }}
@@ -37,3 +49,27 @@ export default function TabNavigator() {
     </Tab.Navigator>
   );
 }
+
+const AddItemButton = () => {
+  return (
+    <TouchableOpacity style={styles.addItemButton}>
+      <Image source={require('@assets/images/plus.png')} style={styles.plusImage} />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  addItemButton: {
+    top: -20,
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: colors.lightPurple,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  plusImage: {
+    width: 30,
+    height: 30,
+  },
+});
--- src/navigator/tab/Tab.typeDefs.ts
+++ src/navigator/tab/Tab.typeDefs.ts
@@ -9,6 +9,7 @@ export type TabBarStatus = {
 export type TabParamList = {
   HomeTab: undefined;
   ProfileTab: undefined;
+  Create: undefined;
   // add more tab params...
 };
 
